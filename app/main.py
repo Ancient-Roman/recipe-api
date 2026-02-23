6b3484caf618f5a5f9ead78bee5feb512054365a
Drew Bowman <55845048+Ancient-Roman@users.noreply.github.com>
2026-02-22 19:19:09 -0500

    Set up CORS policy

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import recipes
 from .database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://example.com",  # Production site
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(recipes.router)
